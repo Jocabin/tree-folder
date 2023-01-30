5acc3314e55c25bf58eddb5612bddbc5f43ca117
Jocabin <kobyfar@hotmail.com>
2023-01-30 18:03:20 +0100

    fix sans parents

--- script.js
+++ script.js
@@ -4,29 +4,23 @@ const tree = [
         name: "Fichier",
         childrens: [
             {
-                parent: 'Fichier',
                 name: "Exporter",
                 childrens: [
                     {
-                        parent: 'Exporter',
                         name: "pour Excel",
                     },
                     {
-                        parent: 'Exporter',
                         name: "pour Google Doc",
                     }
                 ]
             },
             {
-                parent: 'Fichier',
                 name: "Importer",
                 childrens: [
                     {
-                        parent: 'Importer',
                         name: "via PDF",
                     },
                     {
-                        parent: 'Importer',
                         name: "via JPG",
                     }
                 ]
@@ -34,33 +28,26 @@ const tree = [
         ],
     },
     {
-        parent: 'tree-view',
         name: "Options",
         childrens: [
             {
-                parent: 'Options',
                 name: "Option 1",
                 childrens: [
                     {
-                        parent: 'Option 1',
                         name: "Sous-option 1",
                     },
                     {
-                        parent: 'Option 1',
                         name: "sous option 2",
                     }
                 ]
             },
             {
-                parent: 'Options',
                 name: "Jeanpatrick",
                 childrens: [
                     {
-                        parent: 'Jeanpatrick',
                         name: "salade de pomme de terre",
                     },
                     {
-                        parent: 'Jeanpatrick',
                         name: "bonjoir",
                     }
                 ]
@@ -69,39 +56,25 @@ const tree = [
     }
 ]
 
-function create_item(node, parent) {
-    console.log(parent)
-    let li = document.createElement('li')
-    li.innerText = node.name
+function create_item(node, parent_id) {
+    let parent_li = document.createElement('li')
+    parent_li.innerText = node.name
+    document.getElementById(parent_id).appendChild(parent_li)
 
-    document.getElementById(node.parent).appendChild(li)
-}
+    if (node.childrens) {
+        let parent_ul = document.createElement('ul')
 
-function create_parent(node, parent) {
-    console.log(parent)
-    let li = document.createElement('li')
-    let ul_parent = document.createElement('ul')
+        parent_ul.setAttribute('id', node.name)
+        parent_li.appendChild(parent_ul)
 
-    li.innerText = node.name
-    ul_parent.setAttribute('id', node.name)
-
-    li.appendChild(ul_parent)
-
-    document.getElementById(node.parent).appendChild(li)
-}
-
-function create_tree(tree) {
-    let parent = document.getElementById('tree-view')
-
-    for (let node of tree) {
-        if (!node.childrens) {
-            create_item(node)
-        } else {
-            parent = node.parentElement
-            create_parent(node)
-            create_tree(node.childrens)
-        }
+        create_tree(node.childrens, node.name)
     }
 }
 
-create_tree(tree)
+function create_tree(tree, parent_id) {
+    for (let node of tree) {
+        create_item(node, parent_id)
+    }
+}
+
+create_tree(tree, 'tree-view')
